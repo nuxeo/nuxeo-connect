4e1bfdf39c1377d666474f6963993e532e57a3b7
Julien Carsique <jcarsique@nuxeo.com>
2011-11-18 16:43:08 +0100

    add licenses; fix typo

--- nuxeo-connect-client/src/main/java/org/nuxeo/connect/NuxeoConnectClient.java
+++ nuxeo-connect-client/src/main/java/org/nuxeo/connect/NuxeoConnectClient.java
@@ -1,3 +1,17 @@
+/*
+ * (C) Copyright 2010-2011 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
 package org.nuxeo.connect;
 
 import java.io.InputStream;
@@ -85,7 +99,7 @@ public class NuxeoConnectClient {
                 InputStream pomStream = NuxeoConnectClient.class.getClassLoader().getResourceAsStream(
                         "META-INF/maven/org.nuxeo.connect/nuxeo-connect-client/pom.properties");
                 if (pomStream == null) {
-                    buildVersion = "Unknow (no pom)";
+                    buildVersion = "Unknown (no pom)";
                 } else {
                     PropertyResourceBundle prb = new PropertyResourceBundle(
                             pomStream);
