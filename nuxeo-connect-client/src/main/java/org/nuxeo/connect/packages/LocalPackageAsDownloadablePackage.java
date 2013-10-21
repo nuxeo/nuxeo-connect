21d213b9b23bd8dfb99530930752e08cb8d59961
Julien Carsique <jcarsique@nuxeo.com>
2013-10-21 18:07:20 +0200

    LocalPackageAsDownloadablePackage: cleanup

--- nuxeo-connect-client/src/main/java/org/nuxeo/connect/packages/LocalPackageAsDownloadablePackage.java
+++ nuxeo-connect-client/src/main/java/org/nuxeo/connect/packages/LocalPackageAsDownloadablePackage.java
@@ -1,10 +1,10 @@
 /*
- * (C) Copyright 2010-2012 Nuxeo SA (http://nuxeo.com/) and contributors.
+ * (C) Copyright 2010-2013 Nuxeo SA (http://nuxeo.com/) and contributors.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
  * (LGPL) version 2.1 which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/lgpl.html
+ * http://www.gnu.org/licenses/lgpl-2.1.html
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -43,54 +43,67 @@ public class LocalPackageAsDownloadablePackage implements LocalPackage,
         this.localPackage = localPackage;
     }
 
+    @Override
     public PackageData getData() {
         return localPackage.getData();
     }
 
+    @Override
     public File getInstallFile() throws PackageException {
         return localPackage.getInstallFile();
     }
 
+    @Override
     public Form[] getInstallForms() throws PackageException {
         return localPackage.getInstallForms();
     }
 
+    @Override
     public Task getInstallTask() throws PackageException {
         return localPackage.getInstallTask();
     }
 
+    @Override
     public String getLicenseType() {
         return localPackage.getLicenseType();
     }
 
+    @Override
     public String getLicenseUrl() {
         return localPackage.getLicenseUrl();
     }
 
+    @Override
     public String getLicenseContent() throws PackageException {
         return localPackage.getLicenseContent();
     }
 
+    @Override
     public File getUninstallFile() throws PackageException {
         return localPackage.getUninstallFile();
     }
 
+    @Override
     public Form[] getUninstallForms() throws PackageException {
         return localPackage.getUninstallForms();
     }
 
+    @Override
     public Task getUninstallTask() throws PackageException {
         return localPackage.getUninstallTask();
     }
 
+    @Override
     public Form[] getValidationForms() throws PackageException {
         return localPackage.getValidationForms();
     }
 
+    @Override
     public Validator getValidator() throws PackageException {
         return localPackage.getValidator();
     }
 
+    @Override
     @Deprecated
     public void setState(int state) {
         localPackage.setState(state);
@@ -101,114 +114,142 @@ public class LocalPackageAsDownloadablePackage implements LocalPackage,
         localPackage.setState(state);
     }
 
+    @Override
     public String getClassifier() {
         return localPackage.getClassifier();
     }
 
+    @Override
     public PackageDependency[] getDependencies() {
         return localPackage.getDependencies();
     }
 
+    @Override
     public PackageDependency[] getConflicts() {
         return localPackage.getConflicts();
     }
 
+    @Override
     public PackageDependency[] getProvides() {
         return localPackage.getProvides();
     }
 
+    @Override
     public String getDescription() {
         return localPackage.getDescription();
     }
 
+    @Override
     public String getHomePage() {
         return localPackage.getHomePage();
     }
 
+    @Override
     public String getId() {
         return localPackage.getId();
     }
 
+    @Override
     public String getName() {
         return localPackage.getName();
     }
 
+    @Override
     public int getState() {
         return localPackage.getState();
     }
 
+    @Override
     public String[] getTargetPlatforms() {
         return localPackage.getTargetPlatforms();
     }
 
+    @Override
     public String getTitle() {
         return localPackage.getTitle();
     }
 
+    @Override
     public PackageType getType() {
         return localPackage.getType();
     }
 
+    @Override
     public String getVendor() {
         return localPackage.getVendor();
     }
 
+    @Override
     public Version getVersion() {
         return localPackage.getVersion();
     }
 
+    @Override
     public boolean requireTermsAndConditionsAcceptance() {
         return localPackage.requireTermsAndConditionsAcceptance();
     }
 
+    @Override
     public String getTermsAndConditionsContent() throws PackageException {
         return localPackage.getTermsAndConditionsContent();
     }
 
+    @Override
     public ProductionState getProductionState() {
         return localPackage.getProductionState();
     }
 
+    @Override
     public NuxeoValidationState getValidationState() {
         return localPackage.getValidationState();
     }
 
+    @Override
     public boolean supportsHotReload() {
         return localPackage.supportsHotReload();
     }
 
+    @Override
     public boolean isSupported() {
         return localPackage.isSupported();
     }
 
+    @Override
     public boolean isLocal() {
         return true;
     }
 
+    @Override
     public int getCommentsNumber() {
         return 0;
     }
 
+    @Override
     public int getDownloadsCount() {
         return 0;
     }
 
+    @Override
     public String getPictureUrl() {
         return null;
     }
 
+    @Override
     public int getRating() {
         return 0;
     }
 
+    @Override
     public String getSourceDigest() {
         return null;
     }
 
+    @Override
     public long getSourceSize() {
         return 0;
     }
 
+    @Override
     public String getSourceUrl() {
         return null;
     }
