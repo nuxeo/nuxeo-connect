5bc87c0efd2e3cae9f2919ba1741b2dba0c34a0b
Julien Carsique <jcarsique@nuxeo.com>
2014-11-03 15:36:44 +0100

    NXBT-841: fix TestProxyPacResolver.testNuxeocom

--- nuxeo-connect-client/src/test/resources/test-pac/test1_pac.js
+++ nuxeo-connect-client/src/test/resources/test-pac/test1_pac.js
@@ -1,8 +1,6 @@
 function FindProxyForURL(url, host) {
-  if (isInNet(dnsResolve(host), "79.0.0.0", "82.0.0.0")) {
+    if (dnsResolve(host) == "127.0.0.1") {
+        return "PROXY 127.0.0.1";
+    }
     return "DIRECT";
-  }
-  else {
-    return "PROXY 127.0.0.1";
-  }
 }
